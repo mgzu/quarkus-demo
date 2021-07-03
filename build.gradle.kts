12135512540995e82f611c78444e54e8067edb3a
mgzu <2931170625@qq.com>
2021-07-03 21:20:33 +0800

    initial repo

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    kotlin("jvm") version "1.4.32"
+    kotlin("plugin.allopen") version "1.4.32"
+    id("io.quarkus")
+}
+
+// aliyun 镜像有点问题，有依赖找不到
+//allprojects {
+//    repositories {
+//        maven {
+//            setUrl("https://maven.aliyun.com/repository/public")
+//        }
+//        maven {
+//            setUrl("https://maven.aliyun.com/repository/spring")
+//        }
+//        maven {
+//            setUrl("https://maven.aliyun.com/repository/gradle-plugin")
+//        }
+//        mavenCentral()
+//    }
+//}
+
+repositories {
+    mavenCentral()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation("io.quarkus:quarkus-hibernate-orm")
+    implementation("io.quarkus:quarkus-spring-security")
+    implementation("io.quarkus:quarkus-spring-scheduled")
+    implementation("io.quarkus:quarkus-spring-web")
+    implementation("io.quarkus:quarkus-spring-data-jpa")
+    implementation("io.quarkus:quarkus-jdbc-mysql")
+    implementation("io.quarkus:quarkus-spring-boot-properties")
+    implementation("io.quarkus:quarkus-spring-cache")
+    implementation("io.quarkus:quarkus-hibernate-validator")
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("io.quarkus:quarkus-spring-data-rest")
+    implementation("io.quarkus:quarkus-jackson")
+    implementation("io.quarkus:quarkus-spring-di")
+    implementation("io.quarkus:quarkus-spring-cloud-config-client")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.quarkus:quarkus-arc")
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.rest-assured:rest-assured")
+}
+
+group = "com.github"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+allOpen {
+    annotation("javax.ws.rs.Path")
+    annotation("javax.enterprise.context.ApplicationScoped")
+    annotation("io.quarkus.test.junit.QuarkusTest")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlinOptions.javaParameters = true
+}
---
+++ src/native-test/kotlin/com/github/NativeGreetingControllerIT.kt
@@ -0,0 +1,6 @@
+package com.github
+
+import io.quarkus.test.junit.NativeImageTest
+
+@NativeImageTest
+class NativeGreetingControllerIT : GreetingControllerTest()
